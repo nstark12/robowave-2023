36507baacd91eba2792191b0f3d6a05be7c6589c
Marketing Director <nicolestark1103@gmail.com>
2023-06-28 14:09:59 -0500

    finishing touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,31 @@ $(document).ready(function() {
     PowerGlitch.glitch('.glitch', {
         hideOverflow: true,
     })
+
+    // last error date
+    $('[data-lastErrorDate]').each(function() {
+        var errDate = $(this).attr('data-lastErrorDate')
+        errDate = dayjs(errDate);
+        // get today's date as a daysjs object
+        var today = dayjs();
+
+        var diff = today.diff(errDate, 'day');
+        console.log(diff)
+        
+
+        // determine text color class
+        var textClass;
+        if (diff < 5) {
+            textClass = 'text-danger'
+        } else if (diff < 30) {
+            textClass = 'text-warning'
+        } else {
+            textClass = 'text-success'
+        }
+
+        // sets text of el and adds class
+        $(this).text(diff + " days since last error.")
+        .addClass(textClass)
+
+    })
 });
