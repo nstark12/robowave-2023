81a3b6da0fde3d4281d42d0cbb5ddc3f22e93564
Nicole Stark <nicolestark@Nicoles-iMac.attlocal.net>
2023-06-27 21:02:47 -0500

    added fullpage js and typewriter js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+       licenseKey: 'gplv3-license',
+       navigation: true,
+       navigationTooltips: ['Welcome', 'New Robots', 'Used Robots', 'Contact']
+    });
+
+    // typewriter js
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+    for (var i = 0; i < typewriterElements.length; i++) {
+        var currentEl = typewriterElements[i]
+        var innerText = currentEl.innerText
+
+        new Typewriter(currentEl, {
+            loop: true,
+        }).typeString(innerText).start()
+    }
+});
